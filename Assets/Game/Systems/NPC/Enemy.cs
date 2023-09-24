0944ded4ee819e66be34af65a234f6db8b4a65e2
avko <dachi.avkopashvili.1@btu.edu.ge>
2023-09-24 20:22:37 +0400

    added enemy which follows player

--- Assets/Game/Systems/NPC/Enemy.cs
+++ Assets/Game/Systems/NPC/Enemy.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private NavMeshAgent navMeshAgent;
+    [SerializeField] private Animator characterAnor;
+    [SerializeField] private float attackRange = 2f;
+    [SerializeField] private float health = 100f;
+    [SerializeField] private float followRange = 100f;
+    [SerializeField] private Transform player;
+    [SerializeField] private PlayerHealthBar healthBar;
+
     void Start()
     {
-        
+        healthBar.UpdateHealthBar(health, health);
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        characterAnor.SetBool(Constants.Animation.Booleans.IsWalking, true);
         
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+        
+        if (distanceToPlayer <= followRange)
+        {
+            navMeshAgent.SetDestination(player.position);
+            
+            if (distanceToPlayer <= attackRange)
+            {
+                AttackPlayer();
+            }
+        }
+    }
+
+    void AttackPlayer()
+    {
+       //TODO: Attack 
     }
 }
