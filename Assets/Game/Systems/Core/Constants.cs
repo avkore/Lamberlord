0f4ac10d7cad2770018b9ef61d380564aa2d4b74
avko <dachi.avkopashvili.1@btu.edu.ge>
2023-09-24 15:39:02 +0400

    added animations

---
+++ Assets/Game/Systems/Core/Constants.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public static class Constants
+{
+    public static class Tags
+    {
+        public const string Player = "Player";
+    }
+
+    public static class Animation
+    {
+        public static class Booleans
+        {
+            public static readonly int IsWalking = Animator.StringToHash("IsWalking");
+        }
+
+        public static class Floats
+        {
+            public static readonly int MoveSpeed = Animator.StringToHash("MoveSpeed");
+        }
+    }
+    
+
+}
--- Assets/Game/Systems/Player/PlayerController.cs
+++ Assets/Game/Systems/Player/PlayerController.cs
@@ -5,4 +5,10 @@ public class PlayerController : MonoBehaviour
     public bool IsMoving = false;
     [SerializeField] private Animator characterAnor;
     [SerializeField] private MovementController dMovementController;
+    
+    private void Update() {
+        characterAnor.SetBool(Constants.Animation.Booleans.IsWalking, IsMoving);
+		
+        characterAnor.SetFloat(Constants.Animation.Floats.MoveSpeed, Mathf.InverseLerp(0f, 5f, dMovementController.GetRigid.velocity.magnitude) * 1.2f);
+    }
 }
